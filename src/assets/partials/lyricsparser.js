f16f4d0881014a687319837ffdedbc9413fa3530
ashu <ashu56das@gmail.com>
2020-10-25 17:30:25 +0530

    testing

--- src/assets/partials/lyricsparser.js
+++ src/assets/partials/lyricsparser.js
@@ -6,8 +6,7 @@ class hello{
     }
    async getLyricss(){
     try{
-        const data=await axios(`https://www.azlyrics.com/lyrics/${this.singer}/${this.song}.html`);
-        this.test=data;
+        const data=await axios.get(`https://www.azlyrics.com/lyrics/${this.singer}/${this.song}.html`,{ headers: { 'User-Agent': 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/86.0.4240.111' }  } );
         this.dataElement=data.data;
         this.lyrics=this.dataElement.split('<div>')[1].split('</div>')[0];
 
